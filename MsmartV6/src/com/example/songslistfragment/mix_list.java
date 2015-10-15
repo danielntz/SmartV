64e93636df0095f5ff517e59c8e9284506818010
danielntz <danieljetntz@sina.com>
2015-10-15 21:58:50 +0800

    SmartV
    第一次修改

--- MsmartV6/src/com/example/songslistfragment/mix_list.java
+++ MsmartV6/src/com/example/songslistfragment/mix_list.java
@@ -7,13 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class mix_list  extends Fragment{
-	@Override
+	
+	   private ListView listview;
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View fragment3 = inflater.inflate(R.layout.mix_list_layout, container, false);
+		 listview = (ListView)fragment3.findViewById(R.id.gequliebiao);
 		return  fragment3;
 	}
 }
